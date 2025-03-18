3006378f892de03729b522c60d5cfd0e2d4bdc10
Shreyansh0Shukla <shreyanshshukla.693@gmail.com>
2025-03-18 12:02:14 +0530

    Update Data_Analysis.py
    Update the few errors , warning continues, Next step try to find a solution to stop warning

--- Data_Analysis.py
+++ Data_Analysis.py
@@ -4,27 +4,24 @@ import seaborn as sns
 import kagglehub
 import pandas as pd
 
-# Download the dataset
+
 path = kagglehub.dataset_download("yasserh/titanic-dataset")
 
-# Load CSV file into pandas dataframe
 df = pd.read_csv(f"{path}/Titanic-Dataset.csv")
 
-# Display the first few rows
 print(df.head())
 
 
-# Basic info about the dataset
 print(df.info())
 
-# Handling missing values
+
 df.fillna({'Age': df['Age'].median(), 'Embark_town': 'Unknown'}, inplace=True)
 
-# Exploratory Data Analysis (EDA)
+
 print("\nSummary Statistics:\n", df.describe())
 print("\nValue Counts for Passenger:\n", df['Pclass'].value_counts())
 
-# Visualization: Survival rate by class
+
 plt.figure(figsize=(8, 5))
 sns.barplot(x=df['Pclass'], y=df['Survived'], ci=None, palette='viridis')
 plt.title('Survival Rate by Passenger Class')
